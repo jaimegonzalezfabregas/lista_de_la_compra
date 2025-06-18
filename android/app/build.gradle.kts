34f5c204ccb466f516a7b50dad4e2285e65ed1ed
Jaime <jaimegonzalezfabregas@gmail.com>
2025-06-18 23:21:28 +0200

    rename

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.jaimegonzalezfabregas.shoppinglistandplanner"
+    namespace = "com.jaimegonzalezfabregas.shoppinglist"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.jaimegonzalezfabregas.shoppinglistandplanner"
+        applicationId = "com.jaimegonzalezfabregas.shoppinglist"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
