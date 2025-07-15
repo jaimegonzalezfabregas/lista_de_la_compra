8c3ef804344e8cd6c63d20beb7a83ea3a64b62aa
alvaro <alvarogonzalezsotillo@gmail.com>
2025-07-15 18:03:47 +0200

    Added a library for server code

---
+++ packages/lista_de_la_compra_http_server/lib/lista_de_la_compra_http_server.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/lista_de_la_compra_http_server_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ packages/lista_de_la_compra_http_server/lib/src/lista_de_la_compra_http_server_base.dart
@@ -0,0 +1,40 @@
+// TODO: Put public facing types in this file.
+
+typedef VoidCallback = void Function();
+
+
+abstract class VoidEventSource {
+
+  /// Abstract const constructor. This constructor enables subclasses to provide
+  /// const constructors so that they can be used in const expressions.
+  const VoidEventSource();
+
+  /// Register a closure to be called when the object notifies its listeners.
+  void addListener(VoidCallback listener);
+
+  /// Remove a previously registered closure from the list of closures that the
+  /// object notifies.
+  void removeListener(VoidCallback listener);
+
+  void notifyListeners();
+}
+
+mixin class VoidEventSourceMixin implements VoidEventSource {
+
+  final _growableList = <VoidCallback>[];
+
+  @override
+  void addListener(VoidCallback listener) {
+    _growableList.add(listener);
+  }
+
+  @override
+  void removeListener(VoidCallback listener) {
+    _growableList.remove(listener);
+  }
+
+  @override
+  void notifyListeners(){
+    _growableList.forEach( (listener)=> listener() );
+  }
+}
