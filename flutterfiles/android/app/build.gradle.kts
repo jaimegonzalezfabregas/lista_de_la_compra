6533861f712b336638223facfc9a56190b80b30a
Jaime <jaimegonzalezfabregas@gmail.com>
2026-06-07 23:44:36 +0200

    fix PR

--- flutterfiles/android/app/build.gradle.kts
+++ flutterfiles/android/app/build.gradle.kts
@@ -28,11 +28,6 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-
-        
-        ndk.abiFilters.add("arm64-v8a")
-        ndk.abiFilters.add("x86_64")
-        ndk.abiFilters.add("armeabi-v7a")
     }
 
 
